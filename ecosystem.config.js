27eab2e05d8c546777198af2a07d1808bf74a046
NetLive <sas.1800@mail.ru>
2025-05-29 08:14:47 +0300

    Some update before deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       user: "praktikum",
       host: "158.160.100.179",
       ref: "origin/main",
-      repo: "git@github.com:your-name/mesto-backend.git",
+      repo: "git@github.com:NetLive5/nodejs-mesto-project.git",
       path: "/home/praktikum/mesto-backend",
       "post-deploy":
         "npm ci && pm2 reload ecosystem.config.js --env production",
